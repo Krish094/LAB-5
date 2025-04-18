c6c0dc5a1592e3562e5495cce89188c2fc49b079
Krish094 <krish.aghara2006@gmail.com>
2025-04-18 16:39:47 +0530

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,5 @@
+strings = ["hello", "world", "tushar", "kathiriya", "patel"]
+print("Original list of strings:", strings)
+
+uppercase_strings = [string.upper() for string in strings]
+print("List of strings in uppercase:", uppercase_strings)
